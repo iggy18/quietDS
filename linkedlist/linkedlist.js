7ee931fd863e3c1ef45402e2cc50b14248496e2d
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2021-12-30 20:27:08 -0800

    update

--- linkedlist/linkedlist.js
+++ linkedlist/linkedlist.js
@@ -24,9 +24,6 @@ class LinkedList{
 
     show(){
         let currentNode = this.head;
-        //make a bucket to collect values
-        //move down the list and get the value of each node and push it into the list
-        //join all values in array to return as a string
         let valueArr = [];
 
         while(currentNode !== null){
@@ -38,11 +35,5 @@ class LinkedList{
         return `${prettyList} null`;
     }
 }
-
-const ll = new LinkedList();
-ll.insert(1);
-ll.insert(2);
-ll.insert(3);
-ll.show();
 
 module.exports = LinkedList;
