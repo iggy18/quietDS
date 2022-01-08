286286c4336e04a150156bc682070bef03525ab0
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2022-01-08 15:22:40 -0800

    update

--- stack/stack.test.js
+++ stack/stack.test.js
@@ -8,10 +8,20 @@ test('can instantiate an empty stack', ()=>{
     expect(actual).toBe(expected);
 });
 
-// test('can push value into stack', ()=>{
-//     const s = new Stack();
-//     s.push('a');
-//     let actual = s.top.value;
-//     let expected = 'a'
-//     expect(actual).toBe(expected);
-// });
+test('can push value into stack', ()=>{
+    const s = new Stack();
+    s.push('a');
+    let actual = s.top.value;
+    let expected = 'a'
+    expect(actual).toBe(expected);
+});
+
+test('can push value into stack', ()=>{
+    const s = new Stack();
+    s.push('a');
+    s.push('b');
+    s.push('c');
+    let actual = s.top.value;
+    let expected = 'c'
+    expect(actual).toBe(expected);
+});
