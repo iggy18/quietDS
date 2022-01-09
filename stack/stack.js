10a1516e44f2a0c22f1c0d5f3de17ee5fb7ba2a8
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2022-01-08 18:31:03 -0800

    update

--- stack/stack.js
+++ stack/stack.js
@@ -1,3 +1,5 @@
+const { nullableTypeAnnotation } = require("@babel/types");
+
 class Node{
     constructor(value=null){
         this.value = value;
@@ -9,17 +11,37 @@ class Stack{
     constructor(value){
         this.top = null;
     }
-    push(){
-        return;
+    push(value){
+        if(this.isEmpty()){ 
+            this.top = new Node(value);
+        } else {
+            let newNode = new Node(value);
+            newNode.next = this.top;
+            this.top = newNode;
+        }
     }
     pop(){
-        return;
+        if(this.isEmpty()){
+            throw new Error('this stack is empty');
+        } else {
+            let popedVal = this.top.value;
+            this.top = this.top.next;
+            return popedVal;
+        }
     }
     peek(){
-        return;
+        if(this.isEmpty()) {
+            throw new Error('this stack is empty');
+        } else {
+            return this.top.value;
+        }
     }
     isEmpty(){
-        return;
+        if (this.top === null){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
--- stack/stack.test.js
+++ stack/stack.test.js
@@ -1,14 +1,15 @@
+const { S } = require('xmlchars/xml/1.0/ed5');
 const Stack = require('./stack');
 
 
-test('can instantiate an empty stack', ()=>{
+test('STACK: can instantiate an empty stack', ()=>{
     let s = new Stack();
     let actual = s.top;
     let expected = null;
     expect(actual).toBe(expected);
 });
 
-test('can push value into stack', ()=>{
+test('PUSH: can push value into stack', ()=>{
     const s = new Stack();
     s.push('a');
     let actual = s.top.value;
@@ -16,12 +17,95 @@ test('can push value into stack', ()=>{
     expect(actual).toBe(expected);
 });
 
-test('can push value into stack', ()=>{
+test('PUSH: can push multiple values into stack', ()=>{
     const s = new Stack();
     s.push('a');
     s.push('b');
     s.push('c');
     let actual = s.top.value;
     let expected = 'c'
+    expect(actual).toBe(expected);
+});
+
+test('POP: should throw error when poping an empty stack', ()=>{
+    const s = new Stack();
+    s.push('a');
+    s.push('b');
+    s.push('c');
+    s.pop();
+    s.pop();
+    s.pop();
+    expect(() => {
+        s.pop();
+    }).toThrow('this stack is empty');
+});
+
+test('POP: can push multiple values into stack, and pop one to get the second to last entered value', ()=>{
+    const s = new Stack();
+    s.push('a');
+    s.push('b');
+    s.push('c');
+    s.pop();
+    let actual = s.top.value;
+    let expected = 'b';
+    expect(actual).toBe(expected);
+});
+
+test('POP: can push multiple values into stack, and pop one and get the value of the popped node', ()=>{
+    const s = new Stack();
+    s.push('a');
+    s.push('b');
+    s.push('c');
+    let actual = s.pop();
+    let expected = 'c';
+    expect(actual).toBe(expected);
+});
+
+test('ISEMPTY: should be able to push multiple node and pop them all and end up with empty stack', ()=>{
+    const s = new Stack();
+    s.push('a');
+    s.push('c');
+    s.push('b');
+    s.pop();
+    s.pop();
+    s.pop();
+    let actual = s.isEmpty();
+    let expected = true;
+    expect(actual).toBe(expected);
+});
+
+test('ISEMPTY: should return false if stack is not empty', ()=>{
+    const s = new Stack();
+    s.push('a');
+    s.push('c');
+    s.push('b');
+    s.pop();
+    s.pop();
+    let actual = s.isEmpty();
+    let expected = false;
+    expect(actual).toBe(expected);
+});
+
+test('PEEK: should raise exception when called on empty stack', ()=>{
+    const s = new Stack();
+    expect(() => {
+        s.peek();
+    }).toThrow('this stack is empty');
+});
+
+test('PEEK: should return value of node at top of stack', ()=>{
+    const s = new Stack();
+    s.push('this is the value');
+    let actual = s.peek();
+    let expected = 'this is the value';
+    expect(actual).toBe(expected);
+});
+
+test('PEEK: should not remove the node', ()=>{
+    const s = new Stack();
+    s.push('this is the value')
+    s.peek();
+    let actual = s.isEmpty();
+    let expected = false;
     expect(actual).toBe(expected);
 });
